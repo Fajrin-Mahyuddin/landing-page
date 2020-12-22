0321fcaf0736614604570eff71056f5e822e8a94
Fajrin Mahyuddin <fajrin.telkom12@gmail.com>
2020-12-22 23:34:23 +0800

    delete style js

--- style.js
+++
@@ -1,8 +0,0 @@
-var parent = document.getElementById("dropdown-parent");
-var child = document.getElementById("dropdown");
-
-this.parent.addEventListener("click", function (e) {
-  console.log(this);
-  e.preventDefault();
-  child.style.display = "block";
-});
